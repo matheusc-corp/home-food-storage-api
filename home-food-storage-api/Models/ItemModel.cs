6d939be815a716b2cce1c0b809d2ce8dfeb0da40
Matheus Carvalho <matheus.carvalho.corp@gmail.com>
2025-12-24 17:51:30 +0900

    feat: Installed the SQLServer and Design packages of Entityframework Core. Defined models of the purchase and purchase items

---
+++ home-food-storage-api/Models/ItemModel.cs
@@ -0,0 +1,14 @@
+﻿namespace home_food_storage_api.Models
+{
+    public class ItemModel
+    {
+        public int PurchaseId { get; set; }
+        public int ItemId { get; set; }
+        public string ItemName{ get; set; }
+        public string ItemDescription { get; set; }
+        public float ItemWeight { get; set; }
+        public int ItemQuantity { get; set; }
+        public decimal ItemValue { get; set; }
+        public decimal ItemSubtotal { get; set; }
+    }
+}
---
+++ home-food-storage-api/Models/PurchaseModel.cs
@@ -0,0 +1,14 @@
+﻿namespace home_food_storage_api.Models
+{
+    public class PurchaseModel
+    {
+        public int PurchaseId { get; set; }
+        public DateTime PurchaseCreateDate { get; set; }
+        public DateTime PurchaseUpdateDate { get; set; }
+        public string PurchasePlace { get; set; }
+        public string PurchasePlaceCity { get; set; }
+        public List<ItemModel> PurchaseItems { get; set; }
+        public string PurchasePaymentMethod { get; set; }
+        public decimal PurchaseTotal { get; set; }
+    }
+}
