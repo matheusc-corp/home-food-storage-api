c55bb85d77bfb6a8a18fef2262b1d4e78558b6c7
Matheus Carvalho <matheus.carvalho.corp@gmail.com>
2025-12-24 20:05:19 +0900

    feat: Database configuration

---
+++ home-food-storage-api/Context/PurchaseContext.cs
@@ -0,0 +1,16 @@
+﻿using home_food_storage_api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace home_food_storage_api.Context
+{
+    public class PurchaseContext : DbContext
+    {
+        public PurchaseContext(DbContextOptions<PurchaseContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<PurchaseModel> Purchases { get; set; }
+        public DbSet<ItemModel> Items { get; set; }
+    }
+}
