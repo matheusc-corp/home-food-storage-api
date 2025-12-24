8d9956109288ceb3d56efd3f194b200dc053a41b
Matheus Carvalho <matheus.carvalho.corp@gmail.com>
2025-12-24 20:08:16 +0900

    feat: Set the dependence injection for Item Controller

---
+++ home-food-storage-api/Controllers/ItemController.cs
@@ -0,0 +1,24 @@
+﻿using home_food_storage_api.Context;
+using home_food_storage_api.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace home_food_storage_api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ItemController : Controller
+    {
+        private readonly PurchaseContext _context;
+
+        public ItemController(PurchaseContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public IActionResult CreateItem([FromBody] ItemModel item)
+        {
+            return NoContent();
+        }
+    }
+}
